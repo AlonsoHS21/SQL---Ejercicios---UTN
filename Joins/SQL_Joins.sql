bd1d792201a1ac9368583b9070833055c24c3e85
AlonsoHS21 <alonsohua20@gmail.com>
2021-03-14 00:07:07 -0300

    Avanzando con Joins
    Falta el completar Funciones de resumen y subonsultas

--- Joins/SQL_Joins.sql
+++ Joins/SQL_Joins.sql
@@ -97,16 +97,36 @@ Where CAT.Nombre = 'Historia'
 -- Dios cuantos inner join, con una subconsulta seria mas facil jaja
 
 -- 12 - Listado con nombre de idioma, código de curso y código de tipo de idioma. Listar todos los idiomas indistintamente si no tiene cursos relacionados.
+Select I.Nombre,C.ID As [Codigo de curso], IxC.IDTipo As [Codigo de tipo de idioma] From Idiomas As I
+left join Idiomas_x_Curso As IxC on I.ID = IxC.IDIdioma
+left join Cursos As C on C.ID = IxC.IDCurso
 
 -- 13 - Listado con nombre de idioma de todos los idiomas que no tienen cursos relacionados.
 
 -- 14 - Listado con nombres de idioma que figuren como audio de algún curso. Sin repeticiones.
+Select Distinct I.Nombre As [Nombre de idioma],TI.Nombre From Idiomas As I 
+inner join Idiomas_x_Curso As IxC on I.ID = IxC.IDIdioma
+inner join TiposIdioma As TI on TI.ID = IxC.IDTipo
+inner join Cursos As C on C.ID = IxC.IDCurso
+Where TI.Nombre = 'Audio'
 
 -- 15 - Listado con nombres y apellidos de todos los usuarios y el nombre del país en el que nacieron. Listar todos los países indistintamente si no tiene usuarios relacionados.
+Select DP.Nombres + ',' + DP.Apellidos As [Nombre y Apellido de Usuarios],P.Nombre From Datos_Personales As DP 
+inner join Usuarios As U on DP.ID = U.ID
+right join Paises As P on P.ID = DP.IDPais
 
 -- 16 - Listado con nombre de curso, fecha de estreno y nombres de usuario de todos los inscriptos. Listar todos los nombres de usuario indistintamente si no se inscribieron a ningún curso.
+Select C.Nombre,C.Estreno,U.NombreUsuario From Cursos As C
+inner join Inscripciones As I on C.ID = I.IDCurso
+right join Usuarios As U on U.ID = I.IDUsuario
+
+-- La duda viene cuando un usuario puede anotarse a mas de un curso, que esten los Nombre de usuario repetidos supongo que esta correcto
 
 -- 17 - Listado con nombre de usuario, apellido, nombres, género, fecha de nacimiento y mail de todos los usuarios que no cursaron ningún curso.
+Select NombreUsuario,Apellidos,Nombres,Genero,Nacimiento,Email From Datos_Personales As DP 
+inner join Usuarios As U on DP.ID = U.ID
+left join Inscripciones As I on U.ID = I.IDUsuario
+Where I.IDCurso is null
 
 -- 18 - Listado con nombre y apellido, nombre del curso, puntaje otorgado y texto de la reseña. Sólo de aquellos usuarios que hayan realizado una reseña inapropiada.
 
