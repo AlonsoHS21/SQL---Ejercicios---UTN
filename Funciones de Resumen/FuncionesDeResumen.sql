b677b394d42fbefb1ce21b1527ba1fe0d6b3941f
AlonsoHS21 <alonsohua20@gmail.com>
2021-03-15 01:30:46 -0300

    Creando los sql y copiando las consignas a realizar

---
+++ Funciones de Resumen/FuncionesDeResumen.sql
@@ -0,0 +1,22 @@
+use MonkeyUniv
+
+-- 1 - Listado con la cantidad de cursos.-- 2 - Listado con la cantidad de usuarios.-- 3 - Listado con el promedio de costo de certificación de los cursos.-- 4 -  Listado con el promedio general de calificación de reseñas.-- 5 - Listado con la fecha de estreno de curso más antigua.-- 6 - Listado con el costo de certificación menos costoso.-- 7 - Listado con el costo total de todos los cursos.-- 8 -  Listado con la suma total de todos los pagos.-- 9 - Listado con la cantidad de cursos de nivel principiante.-- 10 - Listado con la suma total de todos los pagos realizados en 2019.-- 11 - Listado con la cantidad de usuarios que son instructores-- 12 - Listado con la cantidad de usuarios distintos que se hayan certificado.-- 13 - Listado con el nombre del país y la cantidad de usuarios de cada país.-- 14 - Listado con el apellido y nombres del usuario y el importe más costoso
+-- abonado como pago. Sólo listar aquellos que hayan abonado más de $7500.-- 15 - Listado con el apellido y nombres de usuario y el importe más costoso de
+-- curso al cual se haya inscripto.-- 16 - Listado con el nombre del curso, nombre del nivel, cantidad total de clases y
+-- duración total del curso en minutos.-- 17 - Listado con el nombre del curso y cantidad de contenidos registrados. Sólo
+-- listar aquellos cursos que tengan más de 10 contenidos registrados.-- 18 - Listado con nombre del curso, nombre del idioma y cantidad de tipos de idiomas.-- 19 - Listado con el nombre del curso y cantidad de idiomas distintos disponibles.-- 20 - Listado de categorías de curso y cantidad de cursos asociadas a cada
+-- categoría. Sólo mostrar las categorías con más de 5 cursos.-- 21 - Listado con tipos de contenido y la cantidad de contenidos asociados a cada
+-- tipo. Mostrar también aquellos tipos que no hayan registrado contenidos con
+-- cantidad 0.-- 22 - Listado con Nombre del curso, nivel, año de estreno y el total recaudado en
+-- concepto de inscripciones. Listar también aquellos cursos sin inscripciones
+-- con total igual a $0.-- 23 - Listado con Nombre del curso, costo de cursado y certificación y cantidad de
+-- usuarios distintos inscriptos cuyo costo de cursado sea menor a $10000 y
+-- cuya cantidad de usuarios inscriptos sea menor a 5. Listar también aquellos
+-- cursos sin inscripciones con cantidad 0.-- 24 - Listado con Nombre del curso, fecha de estreno y nombre del nivel del curso
+-- que más recaudó en concepto de certificaciones.-- 25 - Listado con Nombre del idioma del idioma más utilizado como subtítulo.-- 26 - Listado con Nombre del curso y promedio de puntaje de reseñas apropiadas-- 27 - Listado con Nombre de usuario y la cantidad de reseñas inapropiadas que registró.-- 28 - Listado con Nombre del curso, nombre y apellidos de usuarios y la cantidad
+-- de veces que dicho usuario realizó dicho curso. No mostrar cursos y usuarios
+-- que contabilicen cero.-- 29 - Listado con Apellidos y nombres, mail y duración total en concepto de clases
+-- de cursos a los que se haya inscripto. Sólo listar información de aquellos
+-- registros cuya duración total supere los 400 minutos.-- 30 - Listado con nombre del curso y recaudación total. La recaudación total
+-- consiste en la sumatoria de costos de inscripción y de certificación. Listarlos
+-- ordenados de mayor a menor por recaudación
---
+++ Subconsultas/Subconsultas.sql
@@ -0,0 +1,31 @@
+use MonkeyUniv
+
+-- 1 - Listado con apellidos y nombres de los usuarios que no se hayan inscripto a cursos durante el año 2019.
+-- 2 - Listado con apellidos y nombres de los usuarios que se hayan inscripto a cursos pero no hayan realizado ningún pago.
+-- 3 - Listado de países que no tengan usuarios relacionados
+-- 4 - Listado de clases cuya duración sea mayor a la duración promedio
+-- 5 - Listado de contenidos cuyo tamaño sea mayor al tamaño de todos los contenidos de tipo 'Audio de alta calidad'.
+-- 6 - Listado de contenidos cuyo tamaño sea menor al tamaño de algún contenido de tipo 'Audio de alta calidad'.
+-- 7 - Listado con nombre de país y la cantidad de usuarios de género masculino y
+-- la cantidad de usuarios de género femenino que haya registrado.
+
+-- 8 - Listado con apellidos y nombres de los usuarios y la cantidad de inscripciones
+-- realizadas en el 2019 y la cantidad de inscripciones realizadas en el 2020.
+-- 9 - Listado con nombres de los cursos y la cantidad de idiomas de cada tipo. Es
+-- decir, la cantidad de idiomas de audio, la cantidad de subtítulos y la cantidad de texto de video.
+-- 10 - Listado con apellidos y nombres de los usuarios, nombre de usuario y
+-- cantidad de cursos de nivel 'Principiante' que realizó y cantidad de cursos de nivel 'Avanzado' que realizó.
+-- 11 - Listado con nombre de los cursos y la recaudación de inscripciones de
+-- usuarios de género femenino que se inscribieron y la recaudación de
+-- inscripciones de usuarios de género masculino.
+
+-- 12 - Listado con nombre de país de aquellos que hayan registrado más usuarios
+-- de género masculino que de género femenino.
+-- 13 - Listado con nombre de país de aquellos que hayan registrado más usuarios
+-- de género masculino que de género femenino pero que haya registrado al
+-- menos un usuario de género femenino.
+
+-- 14 - Listado de cursos que hayan registrado la misma cantidad de idiomas de audio que de subtítulos.
+-- 15 - Listado de usuarios que hayan realizado más cursos en el año 2018 que en el
+-- 2019 y a su vez más cursos en el año 2019 que en el 2020.
+-- 16 -Listado de apellido y nombres de usuarios que hayan realizado cursos pero nunca se hayan certificado
